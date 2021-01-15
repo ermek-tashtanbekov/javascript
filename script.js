81565308e52559d7e19d242164041045f18269c7
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-01-15 22:41:59 +0600

    let subject unit let dark book added console

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ let country = "Britain";
 let subject = "Distance";
 let unit = "meters";
 
+subject = "Length"
+unit = "kilometers"
+
+subject = "Whiht";
+unit = "cm";
+
+let dark = "Dark  tover";
+let book = "SB";
+
 
 console.log(capital + "is the capital of " + country);
 console.log(subject + " is measured in " + unit);
+console.log(subject + " is measured in " + unit);
+console.log("My faforite book is " + dark +" "+ book)
