e8d142b4a6e91d228bf469074e3366c58afe39a4
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-01-15 22:28:26 +0600

    let capital country  subject  unit consolelog

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ let  third = second;
 let  fouth = third;
 let  fifth = fouth;
 console.log(fifth);
+
+let capital = "london";
+let country = "Britain";
+//
+let subject = "Distance";
+let unit = "meters";
+
+
+console.log(capital + "is the capital of " + country);
+console.log(subject + " is measured in " + unit);
